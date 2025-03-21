680f51287fc1bb92979c35daf9a54f00f7e18086
Armand Mosnier <armosnie@bess-f2r1s1.clusters.42paris.fr>
2025-03-21 19:24:42 +0100

    21/03

---
+++ pipex.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: armosnie <armosnie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/20 11:47:56 by armosnie          #+#    #+#             */
+/*   Updated: 2025/03/20 11:54:01 by armosnie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+# define STDIN 0
+# define STDOUT 1
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <stdio.h>
+
+typedef struct s_struct
+{
+    
+}   s_struct;
+
+// parcing functions
+
+#endif
